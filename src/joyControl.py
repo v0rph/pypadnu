b85e64dc0018a6e4e13e065bb6fac197d8c73099
Hugo Marinho <hugompmarinho@gmail.com>
2013-02-20 17:22:29 +0000

    workaround to temporarily disable unwanted debug output

--- src/joyControl.py
+++ src/joyControl.py
@@ -1,3 +1,5 @@
+import sys
+import os
 import pygame
 import time
 
@@ -9,9 +11,13 @@ def joystickControl(mjoy):
         if (e.type == pygame.JOYAXISMOTION):
 	  print "axis"
           #  horizontal axis
+	  sys.stdout = os.devnull
+	  sys.stderr = os.devnull
           ma0 = mjoy.get_axis(0)
 	  #  vertical axis
           ma1 = mjoy.get_axis(1)
+	  sys.stdout = sys.__stdout__
+	  sys.stderr = sys.__stderr__
           if (ma1 < 0):
             return "Up"
           elif (ma1 > 0):
@@ -21,8 +27,12 @@ def joystickControl(mjoy):
           elif (ma0 > 0):
             return "Right"
         if (e.type == pygame.JOYBUTTONDOWN):
+	  sys.stdout = os.devnull
+	  sys.stderr = os.devnull
           mb0 = mjoy.get_button(0)
           mb2 = mjoy.get_button(2)
+	  sys.stdout = sys.__stdout__
+	  sys.stderr = sys.__stderr__
           if (mb0):
             return "button0"
           if (mb2):
--- src/pypadnu.sh
+++ src/pypadnu.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-  /usr/bin/python ~/pypadnu/src/pypadnu.py
+  /usr/bin/python /home/pi/pypadnu/src/pypadnu.py
+  rm -f /etc/profile.d/pypadnu.sh
   exec login -f pi
